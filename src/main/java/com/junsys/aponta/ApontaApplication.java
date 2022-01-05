5a3f9b7c9046f8c4ec1fee4e57993c91a99e3fce
agn88 <agnaldo.junior@unoesc.edu.br>
2022-01-05 10:58:14 -0300

    Primeiro GIT

---
+++ src/main/java/com/junsys/aponta/ApontaApplication.java
@@ -0,0 +1,13 @@
+package com.junsys.aponta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApontaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junsys/aponta/ApontaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junsys.aponta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApontaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
